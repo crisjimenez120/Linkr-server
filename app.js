20233442ccda6a212ba817b93282900307f70c96
SaifShakur <saifmshakur@gmail.com>
2018-11-16 17:49:33 -0500

    ISH THINKS I DON'T KNOW GIT HUB HURP A DERP

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var Express = require("express");		//Using express
 var Sequelize = require("sequelize");	//Using sequelize
+var router = Express.Router();
+var bodyParser = require('body-parser');
 // var models = require('../models');
 
 //Assuming we're connected to "linkr_development" database
@@ -7,6 +9,7 @@ var sequelize = new Sequelize('postgres://linkr:password@localhost:5432/linkr_de
 var app = Express();	// the express "object" we're using to call routes
 
 var PORT = process.env.PORT || 3001;	//port we usin
+// var models = require('./models');
 
 
 //a simple authentication check, to make sure we in dat ass
@@ -16,6 +19,8 @@ sequelize.authenticate() .then(() => {
     console.error('Unable to connect to the database:', err);
   });
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // sequelize.sync({ force: false })
 //   .then(() => {
@@ -27,24 +32,24 @@ sequelize.authenticate() .then(() => {
 //-------------------------[Models]----------------------------
 
 
-var users = sequelize.define ('users', {
-		user_id: {type: Sequelize.INTEGER, primaryKey: true},
-		user_name: Sequelize.STRING,
-	    password: Sequelize.STRING
-});
+// var users = sequelize.define ('users', {
+// 		user_id: {type: Sequelize.INTEGER, primaryKey: true},
+// 		user_name: Sequelize.STRING,
+// 	    password: Sequelize.STRING
+// });
 
 var events = sequelize.define ('events', {
 		title: Sequelize.STRING,
-		start_date: Sequelize.DATE,
-		end_date: Sequelize.DATE
+		start: Sequelize.DATE,
+		end: Sequelize.DATE
 });
 
-var groups = sequelize.define ('groups', {
-		group_id: Sequelize.INTEGER,
-		user_id: Sequelize.INTEGER,
-		group_name: Sequelize.STRING,
-		admin: Sequelize.BOOLEAN
-});
+// var groups = sequelize.define ('groups', {
+// 		group_id: Sequelize.INTEGER,
+// 		user_id: Sequelize.INTEGER,
+// 		group_name: Sequelize.STRING,
+// 		admin: Sequelize.BOOLEAN
+// });
 
 //NEED TO SYNC TO UPDATE TABLE
 // events.sync();
@@ -65,17 +70,30 @@ app.get("/api_events", (request, response) => {
 });
 
 //basic route to ADD a mock entry
-app.get("/api_create", (request, response) =>{
+app.post("/api_create", (request, response) =>{
 /*
 		{id: 3, title: "Beat All The Gyms", start: "2018-11-15T08:00:00.000Z", end: "2018-11-15T11:00:00.000Z"}
 		{id: 3, title: "Beat All The Gyms", start: "2018-11-15T08:00:00.000Z", end: "2018-11-15T11:00:00.000Z"}
 */
 	events.create({
-		title: "BEATING CRIS IN POKEMON",
-		start_date: "2018-11-15T08:00:00.000Z",
-		end_date: "2018-11-15T11:00:00.000Z",
+
+		// title: "BEATING CRIS IN POKEMON",
+		// start: "2018-11-15T08:00:00.000Z",
+		// end: "2018-11-15T11:00:00.000Z"
+
+		title: request.body.title,
+		start: request.body.start,
+		end: request.body.end
+
+		
+		// title: "BEATING CRIS IN POKEMON",
+		// start: "2018-11-15T08:00:00.000Z",
+		// end: "2018-11-15T11:00:00.000Z",
+		
 	}).then((get) => {
-	  response.redirect('/');
+	  //response.redirect('/api_events');
+	  console.log("SUCCESS, WE ADDED A USER MADE THING");
+	  response.send(200);
 	}).catch((err) => {
       console.log(err);
     });
@@ -92,10 +110,10 @@ app.get("/api_delete", (request, response) =>{
 
 	events.destroy({
 		where: {
-			title: "FUCKING ISH"
+			title: "BEATING CRIS IN POKEMON"
 		}
 	}).then((get) => {
-	  response.redirect('/');
+	  response.redirect('/api_events');
 	}).catch((err) => {
       console.log('ERROR while DELETEING a post for Nartuo');
     });
@@ -107,6 +125,22 @@ app.get("/api_delete", (request, response) =>{
 
 app.get("/api_update", (request, response) =>{
 
+	events.update(
+	{
+		start: "2999-12-21T08:00:00.000Z"
+	},
+	{
+		where:
+		{
+			title: "BEATING CRIS IN POKEMON"
+		}
+	}).then((get) =>{
+		response.redirect("/api_events");
+	}).catch((error) => {
+		console.log("ERROR while UPDATING Naruto's event");
+	})
+
+	/*
 	users.update(
 	{
 		event_name: "MY PARENTS ARE DEAD"
@@ -121,6 +155,7 @@ app.get("/api_update", (request, response) =>{
 	}).catch((error) => {
 		console.log("ERROR while UPDATING Naruto's event");
 	})
+	*/
 
 	// response.json({
 	// 	example: "this is the example"
@@ -132,6 +167,14 @@ app.get("/api_update", (request, response) =>{
 app.listen(PORT, () => {
   console.log(`Issa running on port ${PORT}`);
 });
+
+
+// models.sequelize.sync({ force: false })
+//   .then(() => {
+//     app.listen(PORT, () => {
+//       console.log(`Server is up and running on port: ${PORT}`)
+//     });
+//   })
 //----------------------[Controllers]---------------------------
 
 
