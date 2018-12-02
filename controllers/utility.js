9895f784a9cdf2df80e1e8f9f45dc8386432a4f4
Ishmaelk <kheir1@msn.com>
2018-12-02 17:47:58 -0500

    Created utility.js file that contains MergeIntervals function

---
+++ controllers/utility.js
@@ -0,0 +1,32 @@
+function MergeIntervals (events) {
+	let merged = [];
+	events = BubbleSort(events);
+	for (let i = 0; i < events.size(); i++) {
+		if (merged.size() != 0 && merged[merged.size()-1].end >= events[i].start) {
+			let merge = {title: "merged", start: events[i-1].start, end: MaxEnd (array, i, j)};
+			merged[merged.size()-1].end = merge;
+		} else // no overlap
+			merged.add(events[i]);
+	}
+}
+
+function BubbleSort(array) {  
+   for (let i = 0; i < n-1; i++)       
+       // Last i elements are already in place    
+       for (let j = 0; j < n-i-1; j++)  
+           if (array[j] > array[j+1]) 
+              Swap(array[j], array[j+1]);
+	return array; 
+}
+
+function MaxEnd (array, i, j) {
+	let x = array[i];
+	let y = array[j];
+	return x.end >= y.end ? x.end : y.end;
+}
+
+function Swap (array, i, j) {
+	let tmp = array[i];
+	array[i] = array[j];
+	array[j] = tmp;
+}
